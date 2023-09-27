d852117cd17c50d22ede2452308c47a06cfdc453
davidgani <64977254+davidgani@users.noreply.github.com>
2023-09-27 21:41:09 +0300

    Add front project

---
+++ UserManagementFront/src/components/card/card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+})
+export class CardComponent {
+  @Input() Project: Project = {
+    subject: '',
+    userId: 0,
+    userName: '',
+    targetDate: new Date(),
+    isCompleted: false
+  };
+
+  constructor() { }
+}
+
+export interface Project {
+  subject: string;
+  userId: number;
+  userName: string;
+  targetDate: Date;
+  isCompleted: boolean;
+}
